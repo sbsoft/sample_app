9071ab83ccd0e347c6053ec7dbcf30825baeff24
Serge Boucher <serge.boucher@gmail.com>
2012-08-15 23:38:46 +0200

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,11 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  
+  # Lower Bcrypt's cost function
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
